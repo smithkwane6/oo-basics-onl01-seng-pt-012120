3f30e269e8d8ddc2ba3e92550355ec6b0b487911
Kwane Smith <smithkwane6@gmail.com>
2020-02-07 15:44:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,6 +16,14 @@ class Book
     @author
   end
 
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+  
 end
 
 #book.new("And Then There Were None")
